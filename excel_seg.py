f65d713d48d65b6920c4945949fad205e88975b0
shishir2635 <shishir2635@gmail.com>
2017-12-31 09:38:17 +0530

    Modified the code with list of branches

--- excel_seg.py
+++ excel_seg.py
@@ -2,36 +2,45 @@ import xlrd,xlwt
 
 print('Excel Segregator v1.0 by Ted_Mosby')
 
-workbook = xlrd.open_workbook('Book1.xlsx')
-wb = xlwt.Workbook() 
+branch_list = ['CSE','IT','EEE','ME','CE','MCA','IC','EE','EC']
 
-worksheet = workbook.sheet_by_index(0)
-ws = wb.add_sheet('EEE')
+#taking inputs from the user
 
-arow = list()
-all_students = list()
+for branch in branch_list:
+	workbook = xlrd.open_workbook('Book1.xlsx')
+	wb = xlwt.Workbook() 
 
-# holding all the data in the record table
-for x in range(worksheet.nrows):
-	for y in range(worksheet.ncols):
-		arow.append(worksheet.cell(x,y).value)
-	all_students.append(arow)
-	arow = []
+	worksheet = workbook.sheet_by_index(0)
+	ws = wb.add_sheet('{}'.format(branch))
 
-filter_list = list()
+	arow = list()
+	all_students = list()
 
-for x in range(len(all_students)):
-	cur_rec = all_students[x]
-	if cur_rec[1] == 'EEE':
-		for y in range(len(cur_rec)):
-			arow.append(cur_rec[y])
-
-		filter_list.append(arow)
+	# holding all the data in the record table
+	for x in range(worksheet.nrows):
+		for y in range(worksheet.ncols):
+			arow.append(worksheet.cell(x,y).value)
+		all_students.append(arow)
 		arow = []
 
-for x in range(len(filter_list)):
-	cur_stu = filter_list[x]
-	for y in range(len(cur_stu)):
-		ws.write(x,y,cur_stu[y])
 
-wb.save('CSE.xls')
+	filter_list = list()
+
+	for x in range(len(all_students)):
+		cur_rec = all_students[x]
+		if cur_rec[1] == branch:
+			for y in range(len(cur_rec)):
+				arow.append(cur_rec[y])
+
+			filter_list.append(arow)
+			arow = []
+
+	if len(filter_list) == 0:
+		continue
+
+	for x in range(len(filter_list)):
+		cur_stu = filter_list[x]
+		for y in range(len(cur_stu)):
+			ws.write(x,y,cur_stu[y])
+
+	wb.save('{} - {} records.xls'.format(branch,len(filter_list)))
